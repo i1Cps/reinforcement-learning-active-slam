8176a88e2c0c7d1e99d594391dc5657fd6133631
i1Cps <nohacks2701@gmail.com>
2024-04-28 11:26:47 +0100

    updated erward func

--- src/active_slam_learning/active_slam_learning/learning_environment/reward_function.py
+++ src/active_slam_learning/active_slam_learning/learning_environment/reward_function.py
@@ -8,6 +8,7 @@ def reward_function(
     linear_vel: float,
     d_opt: float | None,
     max_speed: float,
+    log=False,
 ) -> float:
     # If the environment hasnt calculated a D-Optimality score yet, return 0. (The whole reward system is centered around it)
     if d_opt is None:
@@ -27,14 +28,20 @@ def reward_function(
     # print("angular_vel_reward: {}".format(angular_vel_reward))
 
     # Negative Reward
-    collision_reward = -1800 if collided else 0
+    collision_reward = -100 if collided else 0
 
     # Positive Reward
-    goal_reward = 2200 if found_goal else 0
+    goal_reward = 100 if found_goal else 0
 
     # Positive Reward
     d_optimality_reward = np.tanh(eta / d_opt)
-    print(f"D Opt reward: {d_optimality_reward}")
+    # print(f"D Opt reward: {d_optimality_reward}")
+    if log:
+        print(
+            "Rewards: lv: {:.2f} av: {:.2f} d_opt: {:.2f}".format(
+                linear_vel_reward, angular_vel_reward, d_optimality_reward
+            )
+        )
     return (
         angular_vel_reward
         + goal_reward
