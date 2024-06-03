040cccf5214cf20714e291de02ce5da0c4220345
i1Cps <nohacks2701@gmail.com>
2024-06-03 15:48:43 +0100

    polished reward function

--- src/active_slam_learning/active_slam_learning/learning_environment/reward_function.py
+++ src/active_slam_learning/active_slam_learning/learning_environment/reward_function.py
@@ -10,40 +10,58 @@ def reward_function(
     max_speed: float,
     log=False,
 ) -> float:
-    # If the environment hasnt calculated a D-Optimality score yet, return 0. (The whole reward system is centered around it)
+    """
+    Calculate the reward for the agent based on its current state and actions.
+
+    Parameters:
+    - found_goal (bool): Whether the goal has been found.
+    - collided (bool): Whether the robot has collided.
+    - angular_vel (float): The angular velocity of the robot.
+    - linear_vel (float): The linear velocity of the robot.
+    - d_opt (float | None): D-optimality measure for map uncertainty.
+    - max_speed (float): Maximum speed of the robot.
+    - log (bool): Whether to log debug information.
+
+    Returns:
+    - float: The calculated reward.
+    """
+
+    initial_reward = -0.2
+
+    # Negative Reward: Encourage higher linear velocity
+    # Range: [-0.04 , 0] for linear velocity in range [-0.2,0.2]
+    linear_vel_reward = -0.1 * (max_speed - linear_vel)
+
+    # Negative Reward: Penalise higher angular velocity
+    # Range: [-0.0484, 0] for angular velocity in range [-2.2, 2.2]
+    angular_vel_reward = -0.01 * (angular_vel**2)
+
+    # Negative Reward: High penalty for collisions
+    collision_reward = -1 if collided else 0
+
+    # Positive Reward: High reward for reaching the goal
+    goal_reward = 1 if found_goal else 0
+
+    if log:
+        print(d_opt)
+
+    # Positive Intrinsic Reward: Reward for map uncertainty
     if d_opt is None:
-        return 0.0
+        d_optimality_reward = 0
+    else:
+        # Eta is a coefficent for scaling the map uncertainty value
+        eta = 0.01
+        d_optimality_reward = np.tanh(eta / d_opt)
 
-    # Eta is a coefficent for scaling the map uncertainty value, this really should be a constant but eh
-    eta = 0.01
-
-    # Negative Reward
-    linear_vel_reward = -1 * (((max_speed - linear_vel) * 10) ** 2)
-    # self.get_logger().info("linear_vel_reward: {}".format(linear_vel_reward))
-    # print("linear_vel_reward: {}".format(linear_vel_reward))
-
-    # Negative Reward
-    angular_vel_reward = -3 * (angular_vel**2)
-    # self.get_logger().info("angular_vel_reward: {}".format(angular_vel_reward))
-    # print("angular_vel_reward: {}".format(angular_vel_reward))
-
-    # Negative Reward
-    collision_reward = -100 if collided else 0
-
-    # Positive Reward
-    goal_reward = 100 if found_goal else 0
-
-    # Positive Reward
-    d_optimality_reward = np.tanh(eta / d_opt)
-    # print(f"D Opt reward: {d_optimality_reward}")
     if log:
         print(
-            "Rewards: lv: {:.2f} av: {:.2f} d_opt: {:.2f}".format(
+            "Rewards: lv: {:.4f} av: {:.4f} d_opt: {:.4f}".format(
                 linear_vel_reward, angular_vel_reward, d_optimality_reward
             )
         )
     return (
-        angular_vel_reward
+        initial_reward
+        + angular_vel_reward
         + goal_reward
         + collision_reward
         + d_optimality_reward
