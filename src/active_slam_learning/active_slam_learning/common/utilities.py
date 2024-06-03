55ae6ed53e1c521312fdbb2ed0c57e952bd2fb45
i1Cps <nohacks2701@gmail.com>
2024-06-03 15:46:12 +0100

    polished helper functions

--- src/active_slam_learning/active_slam_learning/common/utilities.py
+++ src/active_slam_learning/active_slam_learning/common/utilities.py
@@ -1,64 +1,44 @@
 import rclpy
-import torch
 import numpy as np
 from std_srvs.srv import Empty
 from active_slam_interfaces.srv import StepEnv, ResetEnv
 
 
-# Pause Gazebo Physics simulation
-def pause_simulation(agent_self):
-    while not agent_self.gazebo_pause.wait_for_service(timeout_sec=1.0):
-        agent_self.get_logger().info(
-            "pause gazebo service not available, waiting again..."
-        )
-    future = agent_self.gazebo_pause.call_async(Empty.Request())
-    while rclpy.ok():
-        rclpy.spin_once(agent_self)
-        if future.done():
-            return
-
-
-# Unpause Gazebo Physics simulation
-def unpause_simulation(agent_self):
-    while not agent_self.gazebo_unpause.wait_for_service(timeout_sec=1.0):
-        agent_self.get_logger().info(
-            "unpause gazebo service not available, waiting again..."
-        )
-    future = agent_self.gazebo_unpause.call_async(Empty.Request())
-    while rclpy.ok():
-        rclpy.spin_once(agent_self)
-        if future.done():
-            return
-
-
-# Function communicates with environment node, it request action and expects response
-# containing observation, reward, and whether episode has finished or truncated
-def step(agent_self, action, discrete=False):
+# Communicates to the Learning Environment Node that it should step the environment using these actions
+# It then expects typical reinforcement learning items back
+def step(agent_self, action):
     req = StepEnv.Request()
     actions = np.array((action[0], action[1]), dtype=np.float32)
     req.actions = actions
 
-    while not agent_self.environment_step_client.wait_for_service(timeout_sec=1.0):
+    while not agent_self.step_environment_client.wait_for_service(timeout_sec=1.0):
         agent_self.get_logger().info(
             "environment step service not available, waiting again..."
         )
-    future = agent_self.environment_step_client.call_async(req)
+    future = agent_self.step_environment_client.call_async(req)
 
     while rclpy.ok():
         rclpy.spin_once(agent_self)
         if future.done():
             if future.result() is not None:
                 res = future.result()
-                return res.state, res.reward, res.done, res.truncated
+                info = {
+                    "collided": res.info.collided,
+                    "goal_found": res.info.goal_found,
+                    "distance_to_goal": res.info.distance_to_goal,
+                }
+                if res.info.goal_found:
+                    print(info)
+                    print(info["goal_found"])
+                return res.state, res.reward, res.done, res.truncated, info
             else:
                 agent_self.get_logger().error(
                     "Exception while calling service: {0}".format(future.exception())
                 )
                 print("Error getting step service response, this wont output anywhere")
-    print("next")
 
 
-# Make this spin til complete
+# Communicates to the Learning Environment Node that it should reset everything
 def reset(agent_self):
     req = ResetEnv.Request()
     while not agent_self.reset_environment_client.wait_for_service(timeout_sec=1.0):
@@ -78,3 +58,24 @@ def reset(agent_self):
                     "Exception while calling service: {0}".format(future.exception())
                 )
                 print("Error resetting the env, if thats even possible ")
+
+
+# Communicates to the Learning Environment Node that it should skip a frame
+def skip_frame(agent_self):
+    req = Empty.Request()
+    while not agent_self.skip_environment_frame_client.wait_for_service(
+        timeout_sec=1.0
+    ):
+        agent_self.get_logger().info("Frame skip service not available, waiting ")
+    future = agent_self.skip_environment_frame_client.call_async(req)
+
+    while rclpy.ok():
+        rclpy.spin_once(agent_self)
+        if future.done():
+            if future.result() is not None:
+                return
+            else:
+                agent_self.get_logger().error(
+                    "Exception while calling service: {0}".format(future.exception())
+                )
+                print("Error skipping the env, if thats even possible ")
